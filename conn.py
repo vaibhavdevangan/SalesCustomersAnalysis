5b77680b668085f761702f94eaaf245a4c94c062
vaibhavdevangan <159937455+vaibhavdevangan@users.noreply.github.com>
2026-02-06 00:42:17 +0530

    changes made to the repository.

--- conn.py
+++ conn.py
@@ -3,4 +3,4 @@ from sqlalchemy import create_engine
 engine = create_engine(
     "postgresql+psycopg2://postgres:new_password@127.0.0.1:5432/Superstore_db"
 )
-print("Conn Successfull")
+print("Conn Successfull") 
